7025b133aa5204631821c3b0971e4c1d494e0174
Christoph Pojer <christoph.pojer@gmail.com>
2011-02-27 02:10:48 +0100

    Adding addition security checks - Use stat to check if the provided folder exists - Escape the URL and check if it doesn't contain " or \
    closes #3

--- PluginSource/githubclone/githubcloneAPI.cpp
+++ PluginSource/githubclone/githubcloneAPI.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <sys/stat.h>
 #include "JSObject.h"
 #include "variant_list.h"
 #include "DOM/Document.h"
@@ -8,7 +9,6 @@
 #include "githubcloneAPI.h"
 
 using std::string;
-namespace fs = boost::filesystem;
 
 githubcloneAPI::githubcloneAPI(const githubclonePtr& plugin, const FB::BrowserHostPtr& host) : m_plugin(plugin), m_host(host){
     registerMethod("clone", make_method(this, &githubcloneAPI::clone));  
@@ -22,14 +22,27 @@ githubclonePtr githubcloneAPI::getPlugin(){
     return plugin;
 }
 
-FB::variant githubcloneAPI::clone(const string& folder, const string& URL){
-    // TODO this needs some security (realpath?)
-    string clone = "cd ";
-    clone.append(folder);
-    clone.append(" && git clone ");
-    clone.append(URL);
-    system(clone.c_str());
+bool isDir(const string& path){
+	struct stat info;
+	return stat(path.c_str(), &info) == 0 && S_ISDIR(info.st_mode);
+}
+
+bool isClean(const string& str){
+	return !(str.find("\"") != string::npos || str.find("\\") != string::npos);
+}
+
+FB::variant githubcloneAPI::clone(const string& path, const string& URL){
+	string folder = path;
+	if (folder.compare(0, 1, "~") == 0){
+		folder.erase(0, 1);
+		folder = getenv("HOME") + folder;
+	}
+	
+	// Check if folder is a valid folder and the git URL does not contain invalid chars
+	if (!isDir(folder) || !isClean(URL)) return false;
+
+	folder = "cd \"" + folder + "\" && git clone \"" + URL + "\"";
+	system(folder.c_str());
     
-    FB::variant result = true;
-    return result;
+    return true;
 }
